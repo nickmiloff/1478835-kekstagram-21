4e71bb42146a4c686963ad15fd25b49eddf3d6ca
Nick <nickmiloff@users.noreply.github.com>
2020-10-03 15:28:06 +0300

    Добавляет модуляцию

--- js/pictures.js
+++ js/pictures.js
@@ -1,40 +1,44 @@
 'use strict';
 
-const picturesContainerNode = document.querySelector(`.pictures`);
-const pictureTemplateNode = document.querySelector(`#picture`).content.querySelector(`.picture`);
+{
+  const picturesContainerNode = document.querySelector(`.pictures`);
+  const pictureTemplateNode = document.querySelector(`#picture`).content.querySelector(`.picture`);
 
-function getHtmlPicturesFragment(pictures) {
-  const fragment = document.createDocumentFragment();
+  const getHtmlPicturesFragment = (pictures) => {
+    const fragment = document.createDocumentFragment();
 
-  pictures.forEach((picture) => {
-    const elem = pictureTemplateNode.cloneNode(true);
+    pictures.forEach((picture) => {
+      const elem = pictureTemplateNode.cloneNode(true);
 
-    elem.querySelector(`.picture__img`).src = picture.url;
-    elem.querySelector(`.picture__likes`).textContent = picture.likes;
-    elem.querySelector(`.picture__comments`).textContent = picture.comments.length;
+      elem.querySelector(`.picture__img`).src = picture.url;
+      elem.querySelector(`.picture__likes`).textContent = picture.likes;
+      elem.querySelector(`.picture__comments`).textContent = picture.comments.length;
 
-    fragment.appendChild(elem);
+      fragment.appendChild(elem);
+    });
+
+    return fragment;
+  };
+
+  const indexOfPicture = (picture) => {
+    const picturesNodeList = picturesContainerNode.querySelectorAll(`.picture`);
+    return Array.from(picturesNodeList).indexOf(picture);
+  };
+
+  picturesContainerNode.addEventListener(`click`, (evt) => {
+    const chosenPictureNode = evt.target.closest(`.picture`);
+    if (chosenPictureNode) {
+      const chosenPictureIndex = indexOfPicture(chosenPictureNode);
+
+      window.picture.render(window.picturesArr[chosenPictureIndex]);
+      window.picture.open();
+    }
   });
 
-  return fragment;
+  window.pictures = {
+    init: () => {
+      const picturesHtmlFragment = getHtmlPicturesFragment(window.picturesArr);
+      picturesContainerNode.appendChild(picturesHtmlFragment);
+    }
+  };
 }
-
-function indexOfPicture(picture) {
-  const picturesNodeList = picturesContainerNode.querySelectorAll(`.picture`);
-  return Array.from(picturesNodeList).indexOf(picture);
-}
-
-window.initPictures = () => {
-  const picturesHtmlFragment = getHtmlPicturesFragment(window.picturesArr);
-  picturesContainerNode.appendChild(picturesHtmlFragment);
-};
-
-picturesContainerNode.addEventListener(`click`, (evt) => {
-  const chosenPictureNode = evt.target.closest(`.picture`);
-  if (chosenPictureNode) {
-    const chosenPictureIndex = indexOfPicture(chosenPictureNode);
-
-    window.picture.render(window.picturesArr[chosenPictureIndex]);
-    window.picture.open();
-  }
-});
--- js/preview.js
+++ js/preview.js
@@ -1,61 +1,63 @@
 'use strict';
 
-const previewNodes = {
-  input: document.querySelector(`#upload-file`),
-  modal: document.querySelector(`.img-upload__overlay`),
-  img: document.querySelector(`.img-upload__preview img`),
-  closeButton: document.querySelector(`.img-upload__cancel`),
-  hashtagsInput: document.querySelector(`.text__hashtags`),
-  descriptionTextarea: document.querySelector(`.text__description`)
-};
-
-function previewEscKeydownHandler(evt) {
-  if (window.isEscButton(evt.key) &&
-   document.activeElement !== previewNodes.hashtagsInput &&
-   document.activeElement !== previewNodes.descriptionTextarea) {
-    closePreview();
-  }
-}
-
-function openPreview() {
-  document.body.classList.add(`modal-open`);
-  previewNodes.modal.classList.remove(`hidden`);
-  window.slider.node.classList.add(`hidden`);
-
-  window.setScaleValue();
-
-  document.addEventListener(`keydown`, previewEscKeydownHandler);
-}
-
-function closePreview() {
-  previewNodes.input.value = ``;
-  document.body.classList.remove(`modal-open`);
-  previewNodes.modal.classList.add(`hidden`);
-
-  window.slider.setValue();
-  previewNodes.img.removeAttribute(`style`);
-
-  document.removeEventListener(`keydown`, previewEscKeydownHandler);
-}
-
-previewNodes.input.addEventListener(`change`, () => {
-  const imgFile = previewNodes.input.files[0];
-  const imgReader = new FileReader();
-
-  imgReader.onloadend = () => {
-    previewNodes.img.src = imgReader.result;
-    openPreview();
+{
+  const previewNodes = {
+    input: document.querySelector(`#upload-file`),
+    modal: document.querySelector(`.img-upload__overlay`),
+    img: document.querySelector(`.img-upload__preview img`),
+    closeButton: document.querySelector(`.img-upload__cancel`),
+    hashtagsInput: document.querySelector(`.text__hashtags`),
+    descriptionTextarea: document.querySelector(`.text__description`)
   };
 
-  if (imgFile) {
-    imgReader.readAsDataURL(imgFile);
-  }
-});
+  const previewEscKeydownHandler = (evt) => {
+    if (window.utils.isEscButton(evt.key) &&
+    document.activeElement !== previewNodes.hashtagsInput &&
+    document.activeElement !== previewNodes.descriptionTextarea) {
+      closePreview();
+    }
+  };
 
-previewNodes.closeButton.addEventListener(`click`, closePreview);
+  const openPreview = () => {
+    document.body.classList.add(`modal-open`);
+    previewNodes.modal.classList.remove(`hidden`);
+    window.slider.node.classList.add(`hidden`);
 
-window.preview = {
-  img: previewNodes.img,
-  hashtagsInput: previewNodes.hashtagsInput,
-  descriptionTextarea: previewNodes.descriptionTextarea
-};
+    window.scale.set();
+
+    document.addEventListener(`keydown`, previewEscKeydownHandler);
+  };
+
+  const closePreview = () => {
+    previewNodes.input.value = ``;
+    document.body.classList.remove(`modal-open`);
+    previewNodes.modal.classList.add(`hidden`);
+
+    window.slider.setValue();
+    previewNodes.img.removeAttribute(`style`);
+
+    document.removeEventListener(`keydown`, previewEscKeydownHandler);
+  };
+
+  previewNodes.input.addEventListener(`change`, () => {
+    const imgFile = previewNodes.input.files[0];
+    const imgReader = new FileReader();
+
+    imgReader.onloadend = () => {
+      previewNodes.img.src = imgReader.result;
+      openPreview();
+    };
+
+    if (imgFile) {
+      imgReader.readAsDataURL(imgFile);
+    }
+  });
+
+  previewNodes.closeButton.addEventListener(`click`, closePreview);
+
+  window.preview = {
+    img: previewNodes.img,
+    hashtagsInput: previewNodes.hashtagsInput,
+    descriptionTextarea: previewNodes.descriptionTextarea
+  };
+}
